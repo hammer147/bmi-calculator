577ef80bb537517f304156e0bc63d29b4ffb43f6
Ben Hamerlynck <ben.hamerlynck@telenet.be>
2021-11-21 11:14:10 +0100

    pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest\.json$/]
+  }
+})
+
+// https://github.com/shadowwalker/next-pwa/issues/288#issuecomment-955777098
